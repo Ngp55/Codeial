f644e7cbc0c44d45ddecfbbe634337c5d830b643
Ngp55 <asdfrajkumar1122@gmail.com>
2023-04-12 17:53:39 +0530

    first commit: create dirctory and setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const port = 8000;
+
+
+
+app.listen(port, function(error){
+    if(error){
+        //console.log("Error:",error);
+        //`` this is used inside the console.log() called interpolar
+        console.log(`Error in running the server: ${err}`);
+
+    }
+
+    console.log(`Server is running on port: ${port}`);
+})
