48356279b6141e690bdef471af5a84c2c96e972f
Ngp55 <asdfrajkumar1122@gmail.com>
2023-05-04 12:41:19 +0530

    Home ejs is now showing posts alongside username

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -10,7 +10,7 @@ module.exports.home = function(req,res){
         //                 posts: posts
         //         });
         // });
-        Post.find({}).populate('users').exec(function(err, posts){
+        Post.find({}).populate('user').exec(function(err, posts){
                 return res.render('home',{
                         title:'Codeial || Home',
                         posts: posts
--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -2,6 +2,7 @@ const Post = require('../models/post');
 
 
 module.exports.create = function(req, res){
+    console.log(req.user._id)
     Post.create({
         content: req.body.content,
         user: req.user._id
